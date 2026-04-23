4c1b2d55d8ca20c908254666918d513f559a76c5
awsl233777 <awsl233777@gmail.com>
2026-04-23 13:03:08 +0000

    refactor(proxy): simplify retention helper naming

--- internal/handler/provider_proxy.go
+++ internal/handler/provider_proxy.go
@@ -235,7 +235,7 @@ func (h *ProviderProxyHandler) shouldClearRequestDetailFor(c *flow.Ctx) bool {
 			}
 		}
 	}
-	return h.proxyHandler.executor.ShouldClearRequestDetailFor(devMode)
+	return h.proxyHandler.executor.ShouldClearRequestDetail(devMode)
 }
 
 func generateProxyRequestID() string {
